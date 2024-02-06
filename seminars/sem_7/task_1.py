bb87be6d77c05d1a4b9395e0f6fb0fb151be7062
Egor Menshikov <r3born.fs@gmail.com>
2024-02-06 20:47:49 +0300

    sem_7 started

---
+++ seminars/sem_7/task_1.py
@@ -0,0 +1,21 @@
+"""
+✔ Напишите функцию, которая заполняет файл
+(добавляет в конец) случайными парами чисел.
+✔ Первое число int, второе - float разделены вертикальной чертой.
+✔ Минимальное число - -1000, максимальное - +1000.
+✔ Количество строк и имя файла передаются как аргументы функции.
+
+"""
+from pathlib import Path
+import random
+
+filepath = Path.cwd() / 'Task_1' / 'pairs'
+
+
+def num_pairs(lines: int, filename: Path):
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.writelines([f'{random.randint(-1000, 1000)} | {random.uniform(-1000, 1000)}\n'
+                         for _ in range(lines)])
+
+
+num_pairs(10, filepath)
---
+++ seminars/sem_7/task_2.py
@@ -0,0 +1,32 @@
+"""
+✔ Напишите функцию, которая генерирует
+псевдоимена.
+✔ Имя должно начинаться с заглавной буквы,
+состоять из 4-7 букв, среди которых
+обязательно должны быть гласные.
+✔ Полученные имена сохраните в файл.
+"""
+from pathlib import Path
+import string
+import random
+
+filepath = Path(Path.cwd() / 'Task_2' / 'names')
+
+
+def pseudoname():
+    letters = string.ascii_lowercase
+    vowels = 'aeiou'
+    length = random.randint(4, 7)
+    while True:
+        name = ''.join(random.choice(letters) for _ in range(length))
+        for vowel in vowels:
+            if vowel in name:
+                return name.capitalize()
+
+
+def fill_names(quantity: int, filename: Path):
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.writelines([f'{pseudoname()}\n' for _ in range(quantity)])
+
+
+fill_names(10, filepath)

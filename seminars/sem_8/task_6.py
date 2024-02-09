5ece6835cc9f7a5655c1882ea7133f52d926eb3e
Egor Menshikov <r3born.fs@gmail.com>
2024-02-09 18:53:22 +0300

    sem_8 done

--- seminars/sem_8/task_6.py
+++ seminars/sem_8/task_6.py
@@ -4,3 +4,25 @@
 # 4 этого семинара.
 # Функция должна извлекать ключи словаря для заголовков
 # столбца из переданного файла.
+
+
+import csv
+import pickle
+from pathlib import Path
+
+_PATH_1 = Path.cwd() / 'task_5' / 'users.pickle'
+_PATH_2 = Path.cwd() / 'task_6' / 'users.csv'
+
+
+def convert(source_file: Path = _PATH_1, output_file: Path = _PATH_2):
+    with open(source_file, 'rb') as source, \
+            open(output_file, 'w', newline='') as output:
+        data_list = pickle.load(source)
+        writer = csv.DictWriter(output, fieldnames=data_list[0].keys())
+        writer.writeheader()
+        for item in data_list:
+            writer.writerow(item)
+
+
+if __name__ == '__main__':
+    convert()
--- seminars/sem_8/task_7.py
+++ seminars/sem_8/task_7.py
@@ -1,3 +1,19 @@
 # Прочитайте созданный в прошлом задании csv файл без
 # использования csv.DictReader.
 # Распечатайте его как pickle строку.
+
+import csv
+import pickle
+from pathlib import Path
+
+_PATH = Path.cwd() / 'task_6' / 'users.csv'
+
+
+def csv_print(source_file: Path = _PATH) -> bytes:
+    with open(source_file, 'r', newline='') as source:
+        reader = csv.reader(source)
+        data = [item for item in reader]
+        return pickle.dumps(data)
+
+
+print(csv_print())

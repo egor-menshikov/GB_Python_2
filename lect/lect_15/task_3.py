49ac2b8c4baa647650c897447b55779a9e356b95
Egor Menshikov <r3born.fs@gmail.com>
2024-03-05 15:42:13 +0300

    lect_15 done

--- lect/lect_15/task_3.py
+++ lect/lect_15/task_3.py
@@ -1,2 +1,9 @@
+from collections import namedtuple
+from datetime import date
 
-
+User = namedtuple('User', 'name surname birthday')
+u_1 = User('Egor', 'Menshikov', date(1986, 3, 13))
+print(u_1)
+print(hash(u_1))
+u_2 = User('Egor', 'Menshikov', [1])
+print(hash(u_2))
---
+++ lect/lect_15/task_4.py
@@ -0,0 +1,6 @@
+from array import array, typecodes
+
+a = array('L', [1, 3, 5])
+print(a)
+b = array('L', (1, 3, 5))
+print(a == b)
